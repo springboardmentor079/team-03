1a0eb6ee84b399623602deb7f3932c127ed344bb
unknown <avnichandan2465@gmail.com>
2026-07-12 14:33:49 +0530

    Connect backend to MongoDB Atlas

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -5,6 +6,10 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('BuildTrack backend is running');
